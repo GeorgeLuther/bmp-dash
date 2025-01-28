df6d914b799496196fd269268d40195383a3e67a
George Luther <george.e.luther@gmail.com>
2025-01-28 08:19:50 -0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Example from "../components/Example";
+
+export default function HomePage() {
+  return (
+    <>
+      <Typography>Welcome to Toolpad Core!</Typography>
+      <Example />
+    </>
+  );
+}
