5d276102456adfc80d5a63e196cf56a56536e1da
George Luther <george.e.luther@gmail.com>
2025-11-13 01:27:55 -0500

    continuing to add parts of table template which will contain stuff like tableviewdialog and detail pane

---
+++ src/shared/table/index.tsx
@@ -0,0 +1,1 @@
+//the table will go here it'll take in props from the table feature implementation (personnel, releases) for stuff like columns, data, table settings, but manage the views dialog for us essentially
